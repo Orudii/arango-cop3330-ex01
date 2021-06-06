624211180699b632e54c836f95ba7d932305aa88
Orudii <rudya@knights.ucf.edu>
2021-06-06 03:17:46 -0400

    Initial solution to problem 1

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,16 +1,34 @@
 package base;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Rudy Arango
+ */
+
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    static Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        String name = myApp.readName();
+        String outputString = myApp.generateOutputString(name);
+        myApp.printOutput(outputString);
+    }
+
+    public void printOutput(String outputString) {
+        System.out.println(outputString);
+    }
+
+    public String readName() {
         System.out.print("What is your name? ");
         String name = in.nextLine();
+        return name;
+    }
 
-        String outputString = "Hello, " + name + ", nice to meet you!";
-
-        System.out.println(outputString);
+    public String generateOutputString(String name) {
+        return String.format("Hello, %s, nice to meet you!", name);
     }
 }
